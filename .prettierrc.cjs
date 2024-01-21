26dcc6c8a704ec6e8dcce5349fa05c079d0a3dbf
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2024-01-21 13:41:18 +0530

    Init 2

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-	singleQuote: true,
-	tabWidth: 2,
-	plugins: ['prettier-plugin-svelte'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+  singleQuote: true,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
 };
